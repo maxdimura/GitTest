6ac92936b3c028d5d9e707d2a897fb72432b70d0
Max Dimura <maximdim@macbook-pro15.local>
2019-02-19 10:11:26 +0300

    Initial Commit

---
+++ github fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github fun
+//
+//  Created by Max Dimura on 19/02/2019.
+//  Copyright © 2019 Max Dimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
